d2f08f015bcfcb7a037912c8108462d8f901e23d
Ardjlon <carlosardany@gmail.com>
2026-01-20 10:33:20 +0530

    surya: Remove magisk addon

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -62,6 +62,9 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_DELETE_AROMAFM=1
 	export FOX_ENABLE_APP_MANAGER=1
 
+	#Addons
+	export FOX_DELETE_MAGISK_ADDON=1
+
 	lunch twrp_$FDEVICE-eng
 	# let's see what are our build VARs
 	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
